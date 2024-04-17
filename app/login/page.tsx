a523cc4d59584753fe4a1eceb5430ca22a23f4bf
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-04-17 09:30:16 -0300

    feat

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -10,7 +10,6 @@ export default function Login() {
   const { register, handleSubmit } = useForm<LoginInputs>();
 
   const onSubmit: SubmitHandler<LoginInputs> = (data) => {
-    console.log("Formulario");
     console.log(data);
   };
 
@@ -18,10 +17,12 @@ export default function Login() {
     <div className="flex bg-[#F2EFE5] container h-screen items-center justify-center mx-auto">
       {/*Card*/}
       <form
-        className="flex flex-col bg-white shadow-lg p-5 rounded-lg w-60"
+        className="flex flex-col bg-white shadow-lg p-5 rounded-lg w-64"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <p className="text-black font-semibold mb-3">Bem vindo de volta!</p>
+        <p className="text-black font-semibold mb-3 text-lg">
+          Bem vindo de volta!
+        </p>
         {/*register the input into hook invoking the "register" function*/}
         <input
           className="border-2 mb-2 px-2 py-1 rounded-xl text-sm"
@@ -33,9 +34,10 @@ export default function Login() {
           placeholder="Senha"
           {...register("password")}
         />
+        <p className="text-black text-xs">Preencha corretamente</p>
         <input
           value={"Login"}
-          className="flex self-end bg-green-400 py-1 px-3 rounded-2xl text-xs w-fit"
+          className="flex self-end bg-green-400 font-semibold py-1 px-3 rounded-2xl text-sm w-fit"
           type="submit"
         />
       </form>
