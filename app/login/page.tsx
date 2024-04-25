eec0bf93b64d7697b977fcb1c5f433e2e0952d87
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-04-25 15:01:58 -0300

    feat: css input

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -36,20 +36,18 @@ export default function Login() {
     <div className="flex bg-[#F2EFE5] container h-screen items-center justify-center mx-auto">
       {/*Card*/}
       <form
-        className="flex flex-col bg-white shadow-lg p-5 rounded-lg w-64"
+        className="flex flex-col bg-white shadow-lg p-5 rounded-lg w-60"
         onSubmit={handleSubmit(onSubmit)}
       >
-        <p className="text-black font-semibold mb-3 text-lg">
-          Bem vindo de volta!
-        </p>
+        <p className="text-black font-semibold mb-3">Bem vindo de volta!</p>
         {/*register the input into hook invoking the "register" function*/}
         <input
-          className="border-2 mb-2 px-2 py-1 rounded-xl text-black text-xs"
+          className="text-black text-xs"
           placeholder="E-mail"
           {...register("user")}
         />
         <input
-          className="border-2 mb-3 px-2 py-1 rounded-xl text-black text-xs"
+          className="text-black text-xs"
           placeholder="Senha"
           type="password"
           {...register("password")}
