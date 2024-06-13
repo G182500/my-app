a190ac901a58dfaf0c704169c538e596375ed474
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-06-13 16:45:31 -0300

    g

--- components/products-card.tsx
+++ components/products-card.tsx
@@ -10,13 +10,13 @@ const ProductsCard = ({ title, items }: ProductsCardProps) => {
   return (
     <div className="flex flex-col bg-[#2e2e2e] p-4 md:rounded-lg space-y-4">
       <div className="flex items-center justify-between">
-        <p className="font-semibold text-lg">{title}</p>
+        <p className="font-semibold text-xl">{title}</p>
         <p className="font-semibold opacity-90 text-sm underline underline-offset-4">
           Show all
         </p>
       </div>
 
-      <div className="mt-6 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-2 gap-x-2 gap-y-3">
+      <div className="mt-6 grid grid-cols-1 gap-x-2 gap-y-3 sm:grid-cols-3">
         {items.map((item) => (
           <Product {...item} key={item.id} />
         ))}
