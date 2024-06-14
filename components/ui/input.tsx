16be08290a5bdecf53df291805edca054d26f500
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-06-14 15:24:54 -0300

    i

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,11 +4,11 @@ const Input = () => {
   return (
     <div className="relative flex items-center">
       <input
-        className="bg-[#424242] placeholder:italic font-semibold p-1 pl-2 rounded-lg text-sm text-white placeholder-gray-300"
+        className="bg-[#424242] font-semibold p-1 pl-2 rounded-lg text-sm text-gray-200 placeholder-gray-400"
         placeholder="Search product"
         type="text"
       />
-      <Search className="absolute end-3" color="white" size={18} />
+      <Search className="absolute end-3" color="#9CA3AF" size={18} />
     </div>
   );
 };
