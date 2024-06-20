878e4bc6e064d96d1b910e6ce529f36e81ea16bd
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-06-20 11:28:00 -0300

    a

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <div className="flex flex-col bg-[#1d1d1d] justify-center p-6 mt-2 w-full sm:mt-3">
       <p className="italic opacity-65 text-xs text-center">
-        Developed with Next.js, using Typescript, Tailwind CSS, and Neon
+        Developed with Next.js, using Typescript, Tailwind CSS and Neon
         (PostgreSQL).
       </p>
     </div>
