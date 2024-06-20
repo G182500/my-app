1f05bdce06aed07ee914ddccb5733e9fb7d7255e
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-06-20 15:00:30 -0300

    b

--- components/menu/side-navigation.tsx
+++ components/menu/side-navigation.tsx
@@ -96,59 +96,6 @@ const SideNavigation = () => {
       </div>
     </div>
   );
-  /*
-    <Popover open={isOpen} onOpenChange={close}>
-      <PopoverTrigger className="lg:hidden hover:cursor-pointer" asChild>
-        <MenuIcon size={30} />
-      </PopoverTrigger>
-      <PopoverContent className="flex flex-col bg-[#505050] py-1 space-y-1 w-52 md:w-56">
-        <p className="font-medium pl-3 text-sm text-gray-300 md:text-base">
-          Menu
-        </p>
-        <Separator />
-        <div className="flex flex-col" onClick={close}>
-          <Link href="/" className={linkClass}>
-            <HomeIcon size={20} />
-            <p className={paragraphClass}>Home</p>
-          </Link>
-          <Link href="/cart" className={linkClass}>
-            <ShoppingCart size={20} />
-            <p className={paragraphClass}>Cart</p>
-          </Link>
-          <Link href="/profile" className={linkClass}>
-            <User size={20} />
-            <p className={paragraphClass}>Profile</p>
-          </Link>
-        </div>
-        {admin && (
-          <>
-            <p className="font-medium pl-3 text-sm text-gray-300 md:text-base">
-              Admin
-            </p>
-            <Separator />
-            <div className="flex flex-col" onClick={close}>
-              <Link href="/product/new" className={linkClass}>
-                <SquarePlus size={20} />
-                <p className={paragraphClass}>Create</p>
-              </Link>
-              <Link href="/inventary" className={linkClass}>
-                <GalleryVerticalEnd size={20} />
-                <p className={paragraphClass}>Inventary</p>
-              </Link>
-              <Link href="/users" className={linkClass}>
-                <Users size={20} />
-                <p className={paragraphClass}>Users</p>
-              </Link>
-            </div>
-          </>
-        )}
-        <Separator />
-        <div className={linkClass} onClick={close}>
-          <LogOut size={20} />
-          <p className={paragraphClass}>Exit</p>
-        </div>
-      </PopoverContent>
-    </Popover>*/
 };
 
 export default SideNavigation;
