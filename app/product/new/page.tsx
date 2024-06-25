208add8b064023f524dce707c9a8b54a4ce6ae48
Gabriel Santos Bueno <gasantosbueno@gmail.com>
2024-06-25 14:02:33 -0300

    a

--- app/product/new/page.tsx
+++ app/product/new/page.tsx
@@ -48,65 +48,63 @@ const Create = () => {
   const inputClass = "font-medium text-white placeholder-gray-400";
 
   return (
-    <div className="container mx-auto pt-16 sm:pt-[68px]">
-      <div className="flex flex-col bg-[#1d1d1d] p-4 space-y-4 sm:rounded-lg">
-        <p className="font-semibold text-lg md:text-2xl">New Product</p>
-        <form
-          className="flex flex-col space-y-4"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <input
-            className={`bg-[#424242] p-2 rounded-md w-full ${inputClass}`}
-            placeholder="Title"
-            type="text"
-            {...register("title", { required: true })}
-          />
-          <div className="flex space-x-4">
-            <select
-              className={`bg-[#424242] border-x-4 border-[#424242] rounded-md w-full ${inputClass}`}
-              {...register("category", { required: true })}
-            >
-              <option value="Compact Discs">Compact Discs</option>
-              <option value="Movies">Movies</option>
-              <option value="Posters">Posters</option>
-              <option value="T-Shirts">T-Shirts</option>
-            </select>
-            <div className="flex bg-[#424242] p-2 rounded-md gap-2">
-              <p className="font-medium text-white">R$</p>
-              <input
-                className="bg-transparent font-medium text-white w-24 placeholder:italic"
-                placeholder="1499.90"
-                type="text"
-                {...register("price", {
-                  required: true,
-                })}
-              />
-            </div>
-          </div>
-          <textarea
-            className="flex bg-[#424242] font-medium text-sm text-white py-1 px-2 rounded-md h-40 w-full"
-            {...register("description")}
-            placeholder="Description"
-          />
-          <div className="flex space-x-4">
-            <QuantityInput
-              increment={() =>
-                setValue("quantity", Number(getValues("quantity")) + 1)
-              }
-              decrement={() =>
-                setValue("quantity", Number(getValues("quantity")) - 1)
-              }
-              register={register("quantity", { required: true })}
+    <div className="flex flex-col bg-[#1d1d1d] p-4 space-y-4 sm:rounded-lg">
+      <p className="font-semibold text-lg md:text-2xl">New Product</p>
+      <form
+        className="flex flex-col space-y-4"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className={`bg-[#424242] p-2 rounded-md w-full ${inputClass}`}
+          placeholder="Title"
+          type="text"
+          {...register("title", { required: true })}
+        />
+        <div className="flex space-x-4">
+          <select
+            className={`bg-[#424242] border-x-4 border-[#424242] rounded-md w-full ${inputClass}`}
+            {...register("category", { required: true })}
+          >
+            <option value="Compact Discs">Compact Discs</option>
+            <option value="Movies">Movies</option>
+            <option value="Posters">Posters</option>
+            <option value="T-Shirts">T-Shirts</option>
+          </select>
+          <div className="flex bg-[#424242] p-2 rounded-md gap-2">
+            <p className="font-medium text-white">R$</p>
+            <input
+              className="bg-transparent font-medium text-white w-24 placeholder:italic"
+              placeholder="1499.90"
+              type="text"
+              {...register("price", {
+                required: true,
+              })}
             />
-            <button
-              className="flex bg-[#1b5a8d] font-semibold items-center justify-center gap-2 py-3 rounded-md w-full"
-              type="submit"
-            >
-              SUBMIT
-            </button>
           </div>
-        </form>
-      </div>
+        </div>
+        <textarea
+          className="flex bg-[#424242] font-medium text-sm text-white py-1 px-2 rounded-md h-40 w-full"
+          {...register("description")}
+          placeholder="Description"
+        />
+        <div className="flex space-x-4">
+          <QuantityInput
+            increment={() =>
+              setValue("quantity", Number(getValues("quantity")) + 1)
+            }
+            decrement={() =>
+              setValue("quantity", Number(getValues("quantity")) - 1)
+            }
+            register={register("quantity", { required: true })}
+          />
+          <button
+            className="flex bg-[#1b5a8d] font-semibold items-center justify-center gap-2 py-3 rounded-md w-full"
+            type="submit"
+          >
+            SUBMIT
+          </button>
+        </div>
+      </form>
       <Toaster />
     </div>
   );
